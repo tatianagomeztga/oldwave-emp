837e6f92ee34163ee9a000b54a2a11f8b47e4d4c
Tatiana Gómez <43054844+tatianagomeztga@users.noreply.github.com>
2021-09-20 21:19:59 -0500

    merge ok

---
+++ src/components/slider.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <slide-promos/>
+    <h2 
+      id="lookingfor"
+       align="center"
+       class="mt-8"
+      >¿Qué estás buscando hoy?</h2>
+    <slide-busqueda/>
+  </div>
+</template>
+
+<script>
+  import SlideBusqueda from './SlideBusqueda.vue'
+  import SlidePromos from './SlidePromos.vue'
+
+  export default {
+    components: {
+    "slide-promos": SlidePromos,
+    "slide-busqueda": SlideBusqueda,
+    },
+  }
+</script>
+
+<style>
+@font-face {
+  font-family: "Poppins, SemiBold";
+  src: url("../assets/fonts/Poppins-SemiBold.ttf");
+}
+#lookingfor{
+  font-family: "Poppins, SemiBold";
+  font-size: 35px;
+  color: #772CE8;
+}
+
+
+</style>
