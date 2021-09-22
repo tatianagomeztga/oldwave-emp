5644c70b0c1ac31ddcaab0ef85a74529a9f2a8f4
daniela1409 <ydaniela.osorio@udea.edu.co>
2021-09-21 23:11:35 -0500

    Card detalle del producto

---
+++ src/views/ProductDetail.vue
@@ -0,0 +1,64 @@
+<template >
+  <v-card
+    class="mx-auto my-15"
+    max-width="50%"
+    max-height="90%"
+  >
+  <v-row>
+    <v-container class="col-6">
+      <v-img
+        class="white--text align-end"
+        height="350px"
+        width="100%"
+        src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+      >
+      </v-img>
+    </v-container>
+    <v-row class="col-6">
+      <v-container class="col-6">
+          <h3>Nombre: </h3>
+          <v-sheet>{{ nombre }}</v-sheet>
+      </v-container>
+      <v-container class="col-6">
+          <h3>Marca: </h3>
+          <v-sheet>{{ marca }}</v-sheet>
+      </v-container>
+      <v-container class="col-6">
+          <h3>Ciudad: </h3>
+          <v-sheet>{{ ciudad }}</v-sheet>
+      </v-container>
+      <v-container class="col-6">
+          <h3>Precio: </h3>
+          <v-sheet>{{ precio }}</v-sheet>
+      </v-container>
+      <v-container class="col-12">
+          <h3>Descripción: </h3>
+          <v-sheet>{{ descripcion }}</v-sheet>
+      </v-container>
+    </v-row>
+
+  </v-row>
+  </v-card>
+</template> 
+
+<script>
+export default {
+    name: 'ProductDetail',
+    data () {
+      return {
+        foto: require('../assets/img/iphone11pro.png'),
+        nombre: 'Carro',
+        marca: 'Renault',
+        ciudad: 'Medellin',
+        precio: 30,
+        reseller:[{nombre: 'nombre', Logo: 'logo'}],
+        descripcion: 'Descripcion',
+        rating: 'rating', 
+      }
+    },  
+}
+</script>
+
+<style>
+
+</style>
