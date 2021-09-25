44b1ed68c76369c0f324f64d0d79962b7d23ae66
jhonatan1967 <jhonatana.torres@udea.edu.co>
2021-09-25 01:22:11 -0500

    Update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,7 @@
     <promo></promo>
     <box-features></box-features>    
     <subscribe></subscribe>
+    <v-btn @click=obtener()> Ver </v-btn>
   </div>
 </template>
 
@@ -12,10 +13,23 @@ import slider from '@/components/slider.vue'
 import boxFeatures from "@/components/box-features.vue"
 import promo from "@/components/promo.vue"
 import subscribe from "@/components/subscribe.vue"
+import axios from 'axios'
 
   export default {
     name: 'Home',
-
+    data(){
+      return {
+        datos:[]
+      }
+      
+    },
+    methods: {
+      obtener(){
+        axios.get('http://3.143.212.203/api/v1/search?q=Iphone').then(          
+          response=>{this.datos=response.data})
+          console.log(this.datos)
+      }
+    },
     components: {
       "h-slider": slider,
       "box-features": boxFeatures,
