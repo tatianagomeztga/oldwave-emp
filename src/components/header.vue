e95b84dc09794a8b0f3587692c7824087e859836
jhonatan1967 <jhonatana.torres@udea.edu.co>
2021-09-24 19:34:55 -0500

    Update

--- src/components/header.vue
+++ src/components/header.vue
@@ -72,19 +72,156 @@
         >
         </v-text-field>
       </div>
-       <v-btn
+       <v-btn        
+        @click="dialog = true"
         depressed
         color="white2"
         height="38px"
-        class="violet--text rounded-r-xl rounded-l-0"
+        class="reference violet--text rounded-r-xl rounded-l-0"
         id="btncategories">
-          Todas las categor&iacute;as
+         <span class="reference"> Todas las categor&iacute;as </span>
         <v-img
           src="../assets/icons/Icon-arrow-up-1.png"
           class="shrink ml-2"
           contain>
         </v-img>
       </v-btn>
+  <div class="text-center">
+    <v-dialog
+      v-model="dialog"      
+      width="1200"       
+    >     
+    <div>              
+      <v-card>      
+        <v-card-text>
+          <v-row>
+            <v-col class="px-10 mt-5" cols="3">                      
+              <v-row>                        
+               <strong><span class="reference"> CATEGORIAS SUGERIDAS </span></strong>
+              </v-row>            
+              <v-row>  
+              <v-col> 
+              <br>                           
+                <strong>Celulares</strong>&nbsp;&nbsp;&nbsp;
+                <span class="opacity">Iphone</span> <br>                          
+                <strong>Celulares</strong>&nbsp;&nbsp;&nbsp;                
+                <span class="opacity">Fundas</span> 
+                <br>
+                <strong>Celulares</strong>&nbsp;&nbsp;&nbsp;               
+                <span class="opacity">Cargadores</span>
+                <br>
+                <strong>Celulares</strong>&nbsp;&nbsp;&nbsp;                
+                <span class="opacity">Baterias</span> 
+                <br>              
+                <strong>Celulares</strong>&nbsp;&nbsp;&nbsp;                
+                <span class="opacity">Auriculares</span>  
+                </v-col>               
+              </v-row>                                
+            </v-col>
+              <v-divider
+              vertical
+            ></v-divider>
+            <v-col class="px-10 mt-5" cols="4">
+              <v-row>
+                <span class="reference">PRODUCTOS SUGERIDOS</span>
+              </v-row>
+              <v-row>              
+                <v-col cols="3">
+                <br>
+                  <img  src="../assets/img/iphone1164gb.png"> 
+                </v-col>
+                <v-col>
+                  <v-row>
+                  <br>
+                  Usado
+                  </v-row>
+                  <v-row>
+                    <strong>Apple iPhone 11 64GB</strong>
+                  </v-row>
+                  <v-row>
+                    <span class="reference">$2'300.000</span>
+                  </v-row>
+                </v-col>
+              </v-row>
+
+              <v-row>
+                <v-col cols="3">
+                <img  src="../assets/img/iphonex.png"> 
+                </v-col>
+                <v-col>
+                  <v-row>
+                    Buen estado
+                  </v-row>
+                  <v-row>
+                    <strong>Apple iPhone X 64GB Silver</strong>
+                  </v-row>
+                  <v-row>
+                  <span class="reference">$1'700.000</span>
+                  </v-row>
+                </v-col>
+              </v-row>
+
+              <v-row>
+                <v-col cols="3">
+                <img  src="../assets/img/iphone11pro.png"> 
+                </v-col>
+                <v-col>
+                  <v-row>
+                    Perfecto estado
+                  </v-row>
+                  <v-row>
+                    <strong>Apple iPhone 11 Pro 256GB</strong>
+                  </v-row>
+                  <v-row>
+                  <span class="reference">$3'800.000</span>
+                  </v-row>
+                </v-col>
+              </v-row>
+            </v-col>
+            <v-divider
+              vertical
+            ></v-divider>
+            <v-col class="px-10 mt-5">
+              <v-row>
+                <span class="reference ">RESULTADOS</span>                
+              </v-row>              
+              <v-row>
+                <v-col cols="4" >
+                <br>
+                  <v-row>
+                  <strong>iPhone X</strong>
+                  </v-row>
+                  <v-row>
+                  <strong>iPhone 11</strong>
+                  </v-row>
+                  <v-row>
+                  <strong>Cargador iPhone</strong>
+                  </v-row>
+                </v-col>
+                <v-col>
+                  <img  src="../assets/img/Imagen 82.png"> 
+                </v-col>
+                <v-col>
+                  <v-row>
+                  <strong>iPhone 11 Morado 64Gb</strong>
+                  </v-row>
+                  <v-row>
+                    <span class="reference">$2'800.000</span>
+                  </v-row>
+                  <v-row>
+                    <v-btn class="test white--text rounded-pill format" color="#772CE8" >             
+                      Comprar ahora
+                    </v-btn> 
+                  </v-row>
+                </v-col>
+              </v-row>
+            </v-col>
+          </v-row>
+        </v-card-text>
+      </v-card>
+      </div>
+    </v-dialog>
+  </div>
       <v-spacer></v-spacer>
       <v-btn
         outlined
@@ -111,8 +248,21 @@
     </v-btn>
     </v-app-bar>
   </div>
-  
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        dialog: false,
+      }
+    },
+  }
+</script>
+
+<style>
+@import '../assets/styles/test.css';
+</style>
 
 <style>
   @font-face {
